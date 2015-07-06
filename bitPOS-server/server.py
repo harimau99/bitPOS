cb86160c6321cfdc8541ae74a910dc81ba6ac789
James Gillard <jamesgillard@live.co.uk>
2015-07-06 12:04:17 +0100

    Advantage DB Intregration

--- bitPOS-server/server.py
+++ bitPOS-server/server.py
@@ -5,6 +5,7 @@ from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 import requests
 import json
 import decimal
+import adsdb
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "http://localhost"}})
@@ -64,11 +65,19 @@ def status():
         next_block_fee = rpc_connection.estimatefee(1)
         message = {"maxHeight": max_height, "localHeight": local_height,
                    "connections": connections, 'nextBlockFee': next_block_fee}
-        print(message.values())
         return json.dumps(message, cls=DecimalEncoder)
     except:
         print('Catch this (status)')
         return 'Catch this (status)'
+
+
+@app.route("/db-fetchall")
+def db_fetchall():
+    conn = adsdb.connect(DataSource='c:\\', ServerType='local or remote')
+    cur = conn.cursor()
+    cur.execute('select * from bitPOSdb')
+    cur.fetchall()
+    return json.dumps(cur.fetchall())
 
 
 def get_max_height():
--- modules/salemodule.js
+++ modules/salemodule.js
@@ -1,20 +1,26 @@
 var saleModule = angular.module('saleModule', [])
 
-  .controller('SaleNumpadController', function() {
+  .controller('SaleNumpadController', ['saleService', function(saleService) {
     this.itemcode = '';
+    this.db = [];
     this.keyPress = function(key) {
       this.itemcode += key;
     };
     this.tickPress = function() {
       $('#saletickbutton').css('color', 'green');
+      saleService.dbFetchall().then(function(data){
+            for (var i = 0; i < data.length; i++) {
+              this.db.push(data[i]);
+            };
+        });
     };
-  })
+  }])
 
   .factory('saleService', function($http) {
-    var getDefaultWallet = function() {
-      return $http.get('http://localhost:5000/wallet').then(function(result){
-        return result.data.defaultAddress;
+    var dbFetchall = function() {
+      return $http.get('http://10.211.55.4:5000/db-fetchall').then(function(result){
+        return result.data;
       });
     };
-    return { getDefaultWallet: getDefaultWallet };
+    return { dbFetchall: dbFetchall };
   });
